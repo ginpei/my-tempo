39d9f24cc638ece78a976f4c0a91051a778bbdb9
Ginpei <ginpei@ginpei.dev>
2023-05-21 00:50:57 -0700

    feat: 🎸 button style for disabled and some

--- src/lib/style/Button.tsx
+++ src/lib/style/Button.tsx
@@ -8,9 +8,10 @@ export function Button({ className, ...props }: ButtonProps): JSX.Element {
       Button ${className ?? ''}
       border px-2 h-8
       border-gray-300 rounded-sm
-      hover:bg-gray-100
-      focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-300
+      hover:bg-gray-50
+      focus-visible:bg-gray-50 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-300
       active:bg-gray-200
+      disabled:text-gray-400 disabled:bg-gray-200 disabled:cursor-not-allowed
     `} {...props} />
   );
 }
