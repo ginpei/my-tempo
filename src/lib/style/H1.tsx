47b69858461bc66600616b1c49f7f20acb1980a2
Ginpei <ginpei@ginpei.dev>
2023-05-21 05:04:55 -0700

    style: 💄 fix indent

--- src/lib/style/H1.tsx
+++ src/lib/style/H1.tsx
@@ -6,9 +6,9 @@ export function H1({ className, ...props }: HeadingProps): JSX.Element {
   return (
     <h1
       className={`
-      Button ${className ?? ""}
-      text-3xl font-bold
-    `}
+        Button ${className ?? ""}
+        text-3xl font-bold
+      `}
       {...props}
     />
   );
