8202694c7c37c5abe4b03a9ab91e400c57188366
Ginpei <ginpei@ginpei.dev>
2023-05-21 08:02:57 -0700

    refactor: 💡 simplify

--- src/lib/style/FileInput.tsx
+++ src/lib/style/FileInput.tsx
@@ -31,18 +31,17 @@ export function FileInput({
         flex items-center
         border px-2 h-8
         border-gray-300 rounded-sm
-        [&:hover:not(:disabled,:disabled_&,&.disabled)]:bg-gray-50
-        focus-visible:bg-gray-50 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-300
+        [&:hover:not(:has(:disabled))]:bg-gray-50
+        [&:has(:focus-visible)]:bg-gray-50 [&:has(:focus-visible)]:outline-none [&:has(:focus-visible)]:ring-2 [&:has(:focus-visible)]:ring-gray-300
         active:bg-gray-200
-        [:where(:disabled_&,&.disabled)]:text-gray-400 [:where(:disabled_&,&.disabled)]:bg-gray-200 [:where(:disabled_&,&.disabled)]:cursor-default
-        ${disabled ? "disabled" : ""}
+        [&:has(:disabled)]:text-gray-400 [&:has(:disabled)]:bg-gray-200 [&:has(:disabled)]:cursor-default
       `}
     >
       {label}
       <input
         accept={accept}
         disabled={disabled}
-        className="hidden"
+        className="w-0 h-0"
         multiple={multiple}
         onChange={onInputChange}
         type="file"
