bdc136611ca564ce20fdbc1fce395f77f8576675
Ginpei <ginpei@ginpei.dev>
2023-05-21 07:22:04 -0700

    feat: 🎸 timestamp

--- src/lib/db/dataRecordDb.ts
+++ src/lib/db/dataRecordDb.ts
@@ -1,14 +1,27 @@
-import { DocumentData, DocumentSnapshot } from "firebase/firestore";
+import {
+  DocumentData,
+  DocumentSnapshot,
+  serverTimestamp,
+} from "firebase/firestore";
 import { DataRecord } from "./DataRecord";
 
 export function toDocumentData<T extends DataRecord>(data: T): DocumentData {
-  const { id, ...fields } = data;
-  return fields;
+  const { createdAt, id, ...fields } = data;
+  return {
+    ...fields,
+    createdAt: createdAt === 0 ? serverTimestamp() : createdAt,
+  };
 }
 
 export function ssToDataRecord<T extends DataRecord>(ss: DocumentSnapshot): T {
+  const data = ss.data();
+  if (data === undefined) {
+    throw new Error(`Failed to get data for ID: ${ss.id}`);
+  }
+
   return {
-    ...ss.data(),
+    ...data,
+    createdAt: data["createdAt"].toMillis(),
     id: ss.id,
   } as T;
 }
